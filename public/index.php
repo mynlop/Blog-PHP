e606a8a5f5f293cf1c934b8e58c773dfe4f3b8cc
mynlop <mynlop15@gmail.com>
2017-06-14 15:49:35 -0600

    renderisar vistas desde un metodo

--- public/index.php
+++ public/index.php
@@ -5,10 +5,21 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once '../vendor/autoload.php';
-
 include_once '../config.php';
 
+$baseURL = '';
+$baseDir = str_replace(basename($_SERVER['SCRIPT_NAME']),'', $_SERVER['SCRIPT_NAME']);
+$baseURL = 'http://' . $_SERVER['HTTP_HOST'] . $baseDir;
+define('BASE_URL', $baseURL);
+
 $route = $_GET['route'] ?? '/';
+
+function render($filename, $params = []){
+    ob_start();
+    extract($params);
+    include $filename;
+    return ob_get_clean();
+}
 
 use Phroute\Phroute\RouteCollector;
 
@@ -18,7 +29,7 @@ $router->get('/', function() use ($pdo){
     $query->execute();
 
     $blogPost = $query->fetchAll(PDO::FETCH_ASSOC);
-    include '../Views/index.php';
+    return render('../Views/index.php' , ['blogPost' => $blogPost]);
 });
 
 $dispatcher = new Phroute\Phroute\Dispatcher($router->getData());
