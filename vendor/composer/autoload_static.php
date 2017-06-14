07fc28c39187885564397e6433c25f45fd58fd8c
mynlop <mynlop15@gmail.com>
2017-06-14 12:30:49 -0600

    se instalo phroute con composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInite2e96d33838049cfaad7692255e4adf5
 {
+    public static $prefixLengthsPsr4 = array (
+        'P' => 
+        array (
+            'Phroute\\Phroute\\' => 16,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Phroute\\Phroute\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/phroute/phroute/src/Phroute',
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInite2e96d33838049cfaad7692255e4adf5::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInite2e96d33838049cfaad7692255e4adf5::$prefixDirsPsr4;
 
         }, null, ClassLoader::class);
     }
