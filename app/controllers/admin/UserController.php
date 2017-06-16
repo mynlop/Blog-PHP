d199f9f9be665ff772808e3e6287feedc8c6150b
mynlop <mynlop15@gmail.com>
2017-06-16 12:15:30 -0600

    se agrego vista y controlador para guardar usuarios en la base de datos

--- app/controllers/admin/UserController.php
+++ app/controllers/admin/UserController.php
@@ -4,12 +4,44 @@ namespace  App\Controllers\Admin;
 
 use App\Controllers\BaseController;
 use App\Models\User;
+use Sirius\Validation\Validator;
 
 class UserController extends BaseController{
     public function getIndex(){
         $users = User::all();
         return $this->render('admin/Users.twig',[
             'users' => $users
+        ]);
+    }
+
+    public function getCreate(){
+        return $this->render('admin/insertUser.twig');
+    }
+
+    public function postCreate(){
+        $errors = [];
+        $result = false;
+
+        $validator = new Validator();
+        $validator->add('txtName', 'required');
+        $validator->add('txtEmail', 'required');
+        $validator->add('txtEmail', 'email');
+        $validator->add('txtPassword', 'required');
+        
+        if($validator->validate($_POST)){
+            $user = new User();
+            $user->name = $_POST['txtName'];
+            $user->email = $_POST['txtEmail'];
+            $user->password = password_hash($_POST['txtPassword'], PASSWORD_DEFAULT);
+            $user->save();
+            $result = true;
+        }else{
+            $errors = $validator->getMessages();
+        }
+
+        return $this->render('admin/insertUser.twig',[
+            'result' => $result,
+            'errors' => $errors
         ]);
     }
 }
